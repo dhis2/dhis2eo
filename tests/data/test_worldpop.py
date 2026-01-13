4bdc52e78c526c7b7069b02466590f94efc39d55
Karim Bahgat <karimba@uio.no>
2026-01-13 16:00:56 +0100

    chore: add test for downloading worldpop version 1 and 2

--- tests/data/test_worldpop.py
+++ tests/data/test_worldpop.py
@@ -46,3 +46,50 @@ def test_download_yearly_population_data():
     # test visualize
     #fig = quickplot(ds.sel(time=end))
     #fig.save(dirname / 'quickplot.png')
+
+
+def test_download_yearly_population_versions():
+    # NOTE: in addition to testing the ability to download both global1 and global2 versions
+    # ...this also tests that we can stitch them together to get a single timeseries across the 
+    # ...version break (2015). mostly just for exploration, this may or may not be a good idea
+    # ...due to different methodologies etc.
+
+    # download args
+    dirname = DATA_DIR / '../test_outputs/worldpop'
+    prefix = 'population_yearly_sierra_leone'
+
+    # get country
+    country_code = 'SLE'
+
+    # download global1 data for 2010-2014
+    start = "2010"
+    end = "2014"
+    global1_paths = pop_total.yearly.download(start, end, country_code=country_code, 
+                                            dirname=dirname, prefix=prefix, version='global1')
+    global1_ds = xr.open_mfdataset(global1_paths)
+    logging.info(global1_ds)
+
+    # download global2 data for 2015-2020 (this is the default version)
+    start = "2015"
+    end = "2020"
+    global2_paths = pop_total.yearly.download(start, end, country_code=country_code, 
+                                            dirname=dirname, prefix=prefix) # version='global2'
+    global2_ds = xr.open_mfdataset(global2_paths)
+    logging.info(global2_ds)
+
+    # ensure both versions use the same ordering of x coordinates
+    if global1_ds.x[0] > global1_ds.x[-1]:
+        global1_ds = global1_ds.sortby("x")
+    if global2_ds.x[0] > global2_ds.x[-1]:
+        global2_ds = global2_ds.sortby("x")
+
+    # join the two into one dataset
+    ds = xr.concat([global1_ds, global2_ds], dim="time")
+    logging.info(ds)
+
+    # test total population changes across the version break
+    for yr in range(int(2010), int(2020)+1):
+        ds_year = ds.sel(time=str(yr))
+        total_pop = ds_year['pop_total'].sum().compute().item() # compute+item are needed to execute the dask task
+        logging.info(f'Total population {yr}: {total_pop}')
+
