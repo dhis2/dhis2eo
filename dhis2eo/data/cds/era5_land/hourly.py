4cc8d54a6bc1e876e0ae1a2906686c384da379ce
Karim Bahgat <karimba@uio.no>
2026-02-21 23:00:14 +0100

    chore: initial version of job submission, but turns out era5land uses a queue and only runs 1 at a time

--- dhis2eo/data/cds/era5_land/hourly.py
+++ dhis2eo/data/cds/era5_land/hourly.py
@@ -1,11 +1,13 @@
 import calendar
 import json
 import logging
-from pathlib import Path
 import os
+import time
 from datetime import date, timedelta
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
 
-import earthkit.data
+from ecmwf.datastores import Client
 import xarray as xr
 
 from ...utils import force_logging
@@ -16,13 +18,8 @@ logger = logging.getLogger(__name__)
 force_logging(logger)
 
 
-# Try to fix CDS cache issue by setting download threads to 1
-config = earthkit.data.config
-config.set("number-of-download-threads", 1)
-
-
 # Internal function to execute a single monthly file download (API only allows one month at a time)
-def fetch_month(year, month, bbox, variables):
+def submit_month(client, year, month, bbox, variables):
     # extract the coordinates from input bounding box
     xmin, ymin, xmax, ymax = map(float, bbox)
 
@@ -44,32 +41,18 @@ def fetch_month(year, month, bbox, variables):
     # download the data with earthkit
     logger.info("Downloading data from CDS API...")
     logger.info(f"Request parameters: \n{json.dumps(params)}")
-    data = earthkit.data.from_source("cds", "reanalysis-era5-land", **params)
+    remote = client.submit(
+        "reanalysis-era5-land",
+        params
+    )
 
-    # load lazily from disk using xarray
-    ds = xr.open_dataset(data.path)
-
-    # return
-    return ds
+    return remote
 
 
-# Public API to retrieve data for bbox between start and end date
-def download(
-    start: DateLike,
-    end: DateLike,
-    bbox: BBox,
-    dirname: str,
-    prefix: str,
-    variables: list[str],
-    overwrite: bool = False,
-):
-    """
-    Retrieves ERA5-Land hourly climate data for a given bbox, variables, and start/end dates.
-    Saves to disk in monthly files, as specified by dirname and prefix.
-    Returns list of file paths where data was downloaded, e.g. to use with xr.open_mfdataset().
-    """
+def submit(client, start, end, bbox, dirname, prefix, variables, overwrite):
     os.makedirs(dirname, exist_ok=True)
 
+    # Parse dates
     start_year, start_month = map(int, start.split('-')[:2])
     end_year, end_month = map(int, end.split('-')[:2])
 
@@ -80,6 +63,7 @@ def download(
     last_updated_date = current_date - timedelta(days=7)
 
     files = []
+    request_ids = []
     for year, month in iter_months(start_year, start_month, end_year, end_month):
         logger.info(f'Month {year}-{month}')
 
@@ -106,13 +90,99 @@ def download(
         if overwrite is False and save_path.exists():
             # File already exist, load from file instead
             logger.info(f'File already downloaded: {save_path}')
+            request_ids.append(None)
         
         else:
-            # Download the data
-            ds = fetch_month(year=year, month=month, bbox=bbox, variables=variables)
-                
-            # Save to target path
-            ds.to_netcdf(save_path)
+            # Submit month request
+            remote = submit_month(client, year=year, month=month, bbox=bbox, variables=variables)
+            request_ids.append(remote.request_id)
 
-    # return list of all file downloads
-    return files
+    # return
+    results = list(zip(files, request_ids))
+    return results
+
+
+def download_to_path(remote, filepath):
+    logger.info('Request ready, downloading to', filepath)
+
+    # download to path
+    remote.download(filepath)
+
+    # finished
+    logger.info('Finished downloading to', filepath)
+
+
+# Public API to retrieve data for bbox between start and end date
+def download(
+    start: DateLike,
+    end: DateLike,
+    bbox: BBox,
+    dirname: str,
+    prefix: str,
+    variables: list[str],
+    overwrite: bool = False,
+):
+    """
+    Retrieves ERA5-Land hourly climate data for a given bbox, variables, and start/end dates.
+    Saves to disk in monthly files, as specified by dirname and prefix.
+    Returns list of file paths where data was downloaded, e.g. to use with xr.open_mfdataset().
+    """
+    # create ecmwf client
+    client = Client()
+    client.check_authentication()
+
+    # submit or get from cache
+    results = submit(client, start, end, bbox, dirname, prefix, variables, overwrite=overwrite)
+    
+    # check how many files to download
+    total_downloads = sum([1 for filepath,request_id in results if request_id])
+
+    # download files if needed
+    if total_downloads:
+        max_downloads = 5
+        multi_downloader = ThreadPoolExecutor(max_workers=max_downloads)
+
+        # continuously check and collect results
+        while True:
+            # check for any remaining request ids
+            remaining = [
+                (i, filepath, client.get_remote(request_id)) 
+                for i,(filepath,request_id) in enumerate(results)
+                if request_id
+            ]
+
+            if remaining:
+                logger.info(f'Progress: {total_downloads - len(remaining)} of {total_downloads} job requests finished')
+
+                # get list of ready results
+                ready = [
+                    (i, filepath, remote)
+                    for i,filepath,remote in remaining
+                    if remote.results_ready
+                ]
+
+                # process first ready results if available
+                if ready:
+                    # get first of the ready
+                    i, filepath, remote = ready[0]
+                    
+                    # download the file
+                    multi_downloader.submit(download_to_path, remote, filepath)
+                
+                    # set request id to None to indicate that job is no longer running
+                    results[i] = (filepath, None)
+
+                else:
+                    # all remaining are still processing
+                    # take a break before checking again
+                    time.sleep(15)
+                    
+            else:
+                # stop checking if no remaining request ids
+                logger.info(f'All {total_downloads} job requests completed, waiting for downloads to finish')
+                multi_downloader.shutdown(wait=True)
+                break
+    
+    # return all local filepaths to user
+    filepaths = [filepath for filepath,request_id in results]
+    return filepaths
