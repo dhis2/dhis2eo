25c27470713caa18e5c6a45c60c54f4b7add78c3
Karim Bahgat <karimba@uio.no>
2026-02-26 22:56:14 +0100

    allow cordex bbox extraction (incl 0-360), begin europe support

--- dhis2eo/data/cds/cordex/daily.py
+++ dhis2eo/data/cds/cordex/daily.py
@@ -5,6 +5,7 @@ import shutil
 from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor
 
+import xarray as xr
 from ecmwf.datastores import Client
 
 # TODO: rcm 'ICTP-RegCM4-7' uses yearly rather than 5-yearly downloads, need to handle differently... 
@@ -15,7 +16,7 @@ ALLOWED_END_YEARS = list(range(2010, 2095+1, 5))
 def model_name_to_stub(name):
     return name.replace('-', '_').lower()
 
-def submit_year_block(client, start_year, end_year, region, save_path, variables, scenario, resolution, gcm_model, rcm_model):
+def submit_year_block(client, start_year, end_year, region, save_path, variables, scenario, resolution, gcm_model, rcm_model, ensemble_member):
     '''Submits single year block as separate job in order to get better control over downloaded file names'''
     # construct the query parameters
     params = {
@@ -26,7 +27,7 @@ def submit_year_block(client, start_year, end_year, region, save_path, variables
         "variable": variables,
         "gcm_model": model_name_to_stub(gcm_model),
         "rcm_model": model_name_to_stub(rcm_model),
-        "ensemble_member": "r1i1p1",
+        "ensemble_member": ensemble_member,
         "start_year": [str(start_year)],
         "end_year": [str(end_year)],
         #"download_format": "unarchived", # seems like this is unsupported, so all files are retrieved as zipfiles and have to be extracted
@@ -42,7 +43,7 @@ def submit_year_block(client, start_year, end_year, region, save_path, variables
 
     return remote
 
-def submit(client, start_date, end_date, region, dirname, prefix, variables, scenario, resolution, models, overwrite=False):
+def submit(client, start_date, end_date, region, dirname, prefix, variables, scenario, resolution, models, bbox=None, overwrite=False):
     '''
     Submits all requested year and model combinations as separate jobs.
     Returns immediately with list of (filename,job_id) pairs.
@@ -56,12 +57,14 @@ def submit(client, start_date, end_date, region, dirname, prefix, variables, sce
     files = []
     request_ids = []
     for model in models:
-        gcm_model = model['gcm']
-        rcm_model = model['rcm']
+        gcm_model = model_name_to_stub(model['gcm'])
+        rcm_model = model_name_to_stub(model['rcm'])
+        ensemble_member = model['ens']
         print(f'GCM model {gcm_model}')
         print(f'RCM model {rcm_model}')
+        print(f'Ensemble {ensemble_member}')
 
-        if model_name_to_stub(rcm_model) in SKIP_RCMS:
+        if rcm_model in SKIP_RCMS:
             # HACKY SKIP FOR NOW, SKIP_RCMS IS GLOBAL VARIABLE
             print('Skipping RCM...')
             continue
@@ -70,7 +73,8 @@ def submit(client, start_date, end_date, region, dirname, prefix, variables, sce
             print(f'Years {start_year} to {end_year}')
             
             # TODO: download file should use standardized model stub names
-            out_path = Path(dirname).resolve() / f'{prefix}_{gcm_model}_{rcm_model}_{start_year}-{end_year}.nc'
+            out_name = f'{prefix}_{gcm_model}_{rcm_model}_{start_year}-{end_year}.nc'
+            out_path = Path(dirname).resolve() / out_name
             
             if overwrite is False and out_path.exists():
                 print('File already exists, reusing from cache', out_path)
@@ -80,7 +84,7 @@ def submit(client, start_date, end_date, region, dirname, prefix, variables, sce
             
             else:
                 files.append(out_path)
-                remote = submit_year_block(client, start_year, end_year, region, out_path, variables, scenario, resolution, gcm_model, rcm_model)
+                remote = submit_year_block(client, start_year, end_year, region, out_path, variables, scenario, resolution, gcm_model, rcm_model, ensemble_member)
                 request_ids.append(remote.request_id)
 
     # return
@@ -91,7 +95,8 @@ def extract_file(remote, filepath, bbox=None):
     print('Request ready, downloading to', filepath)
 
     # download as a zipfile
-    filepath_zip = Path(filepath).with_suffix('.zip')
+    filepath = Path(filepath)
+    filepath_zip = filepath.with_suffix('.zip')
     remote.download(filepath_zip)
 
     # extract file from zipfile to target filename
@@ -103,12 +108,39 @@ def extract_file(remote, filepath, bbox=None):
         with zobj.open(first_file) as source, open(filepath, "wb") as target:
             shutil.copyfileobj(source, target)
 
-    # if bbox, open and crop to bbox incl edge padding
-    # also have to detect 0-360 though, and i guess then maybe alter the file coords
-    # ... 
-
     # delete the zipfile afterwards
     filepath_zip.unlink()
+
+    # if bbox given, open and crop to bbox incl edge padding
+    if bbox is not None:
+        # open dataset
+        d = xr.open_dataset(filepath)
+
+        # fix datasets with lon 0-360
+        # TODO: this likely won't detect 0-360 for South American domain? 
+        if d.lon.max() > 180:
+            lon = ((d.lon + 180) % 360) - 180
+            d = d.assign_coords(lon=lon)
+            print('Longitude coordinates converted from 0 to 360, to -180 to 180')
+
+        # subset to bbox
+        xmin,ymin,xmax,ymax = bbox
+        xres = abs(d.lon.max() - d.lon.min()) / len(d.rlon)
+        yres = abs(d.lat.max() - d.lat.min()) / len(d.rlat)
+        mask = (
+            (d.lat >= (ymin-yres)) & (d.lat <= (ymax+yres)) &
+            (d.lon >= (xmin-xres))  & (d.lon <= (xmax+xres))
+        ).compute()
+        d = d.where(mask, drop=True).compute()
+
+        # save to <filepath>_bbox.nc
+        filepath_bbox = filepath.with_suffix('_bbox.nc')
+        d.to_netcdf(filepath_bbox)
+
+        # replace the full file with the bbox subset
+        filepath.unlink()
+        shutil.move(filepath_bbox, filepath)
+        print('Cropped to bbox')
 
     # finished
     print('Finished downloading to', filepath)
--- dhis2eo/data/cds/cordex/models.py
+++ dhis2eo/data/cds/cordex/models.py
@@ -7,31 +7,50 @@ MODELS = {
     'africa': {
         '0_22_degree_x_0_22_degree': {
             'rcp_2_6': [
-                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'CLMcom-KIT-CCLM5-0-15'},
-                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'GERICS-REMO2015'},
-                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'ICTP-RegCM4-7'},
-                {'gcm': 'MPI-M-MPI-ESM-MR', 'rcm': 'ICTP-RegCM4-7'},
-                {'gcm': 'NCC-NorESM1-M', 'rcm': 'CLMcom-KIT-CCLM5-0-15'},
-                {'gcm': 'NCC-NorESM1-M', 'rcm': 'GERICS-REMO2015'},
-                {'gcm': 'NCC-NorESM1-M', 'rcm': 'ICTP-RegCM4-7'},
-                {'gcm': 'MPI-M-MPI-ESM-LR', 'rcm': 'CLMcom-KIT-CCLM5-0-15'},
-                {'gcm': 'MPI-M-MPI-ESM-LR', 'rcm': 'GERICS-REMO2015'},
+                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'CLMcom-KIT-CCLM5-0-15', 'ens': 'r1i1p1'},
+                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'GERICS-REMO2015', 'ens': 'r1i1p1'},
+                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'ICTP-RegCM4-7', 'ens': 'r1i1p1'},
+                {'gcm': 'MPI-M-MPI-ESM-MR', 'rcm': 'ICTP-RegCM4-7', 'ens': 'r1i1p1'},
+                {'gcm': 'NCC-NorESM1-M', 'rcm': 'CLMcom-KIT-CCLM5-0-15', 'ens': 'r1i1p1'},
+                {'gcm': 'NCC-NorESM1-M', 'rcm': 'GERICS-REMO2015', 'ens': 'r1i1p1'},
+                {'gcm': 'NCC-NorESM1-M', 'rcm': 'ICTP-RegCM4-7', 'ens': 'r1i1p1'},
+                {'gcm': 'MPI-M-MPI-ESM-LR', 'rcm': 'CLMcom-KIT-CCLM5-0-15', 'ens': 'r1i1p1'},
+                {'gcm': 'MPI-M-MPI-ESM-LR', 'rcm': 'GERICS-REMO2015', 'ens': 'r1i1p1'},
             ],
             'rcp_4_5': [
-                {'gcm': 'CCCma-CanESM2', 'rcm': 'CCCma-CanRCM4'},
+                {'gcm': 'CCCma-CanESM2', 'rcm': 'CCCma-CanRCM4', 'ens': 'r1i1p1'},
             ],
             'rcp_8_5': [
-                {'gcm': 'CCCma-CanESM2', 'rcm': 'CCCma-CanRCM4'},
-                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'CLMcom-KIT-CCLM5-0-15'},
-                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'GERICS-REMO2015'},
-                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'ICTP-RegCM4-7'},
-                {'gcm': 'MPI-M-MPI-ESM-MR', 'rcm': 'ICTP-RegCM4-7'},
-                {'gcm': 'NCC-NorESM1-M', 'rcm': 'CLMcom-KIT-CCLM5-0-15'},
-                {'gcm': 'NCC-NorESM1-M', 'rcm': 'GERICS-REMO2015'},
-                {'gcm': 'NCC-NorESM1-M', 'rcm': 'ICTP-RegCM4-7'},
-                {'gcm': 'MPI-M-MPI-ESM-LR', 'rcm': 'CLMcom-KIT-CCLM5-0-15'},
-                {'gcm': 'MPI-M-MPI-ESM-LR', 'rcm': 'GERICS-REMO2015'},
+                {'gcm': 'CCCma-CanESM2', 'rcm': 'CCCma-CanRCM4', 'ens': 'r1i1p1'},
+                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'CLMcom-KIT-CCLM5-0-15', 'ens': 'r1i1p1'},
+                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'GERICS-REMO2015', 'ens': 'r1i1p1'},
+                {'gcm': 'MOHC-HadGEM2-ES', 'rcm': 'ICTP-RegCM4-7', 'ens': 'r1i1p1'},
+                {'gcm': 'MPI-M-MPI-ESM-MR', 'rcm': 'ICTP-RegCM4-7', 'ens': 'r1i1p1'},
+                {'gcm': 'NCC-NorESM1-M', 'rcm': 'CLMcom-KIT-CCLM5-0-15', 'ens': 'r1i1p1'},
+                {'gcm': 'NCC-NorESM1-M', 'rcm': 'GERICS-REMO2015', 'ens': 'r1i1p1'},
+                {'gcm': 'NCC-NorESM1-M', 'rcm': 'ICTP-RegCM4-7', 'ens': 'r1i1p1'},
+                {'gcm': 'MPI-M-MPI-ESM-LR', 'rcm': 'CLMcom-KIT-CCLM5-0-15', 'ens': 'r1i1p1'},
+                {'gcm': 'MPI-M-MPI-ESM-LR', 'rcm': 'GERICS-REMO2015', 'ens': 'r1i1p1'},
             ],
         }
-    }
+    },
+    'europe': {
+        '0_11_degree_x_0_11_degree': {
+            'rcp_2_6': [
+                {'gcm': 'ICHEC-EC-EARTH', 'rcm': 'GERICS-REMO2015', 'ens': 'r12i1p1'},
+                {'gcm': 'ICHEC-EC-EARTH', 'rcm': 'SMHI-RCA4', 'ens': 'r12i1p1'},
+                {'gcm': 'ICHEC-EC-EARTH', 'rcm': 'CLMcom-CLM-CCLM4-8-17', 'ens': 'r12i1p1'},
+                {'gcm': 'ICHEC-EC-EARTH', 'rcm': 'DMI-HIRHAM5', 'ens': 'r3i1p1'},
+                {'gcm': 'ICHEC-EC-EARTH', 'rcm': 'KNMI-RACMO22E', 'ens': 'r12i1p1'},
+                {'gcm': 'ICHEC-EC-EARTH', 'rcm': 'MOHC-HadREM3-GA7-05', 'ens': 'r12i1p1'},
+
+                {'gcm': 'GERICS-REMO2015', 'rcm': 'GERICS-REMO2015', 'ens': 'r1i1p1'},
+                {'gcm': 'GERICS-REMO2015', 'rcm': 'ICTP-RegCM4-6', 'ens': 'r1i1p1'},
+                {'gcm': 'GERICS-REMO2015', 'rcm': 'SMHI-RCA4', 'ens': 'r1i1p1'},
+                {'gcm': 'GERICS-REMO2015', 'rcm': 'DMI-HIRHAM5', 'ens': 'r1i1p1'},
+                {'gcm': 'GERICS-REMO2015', 'rcm': 'KNMI-RACMO22E', 'ens': 'r1i1p1'},
+                {'gcm': 'GERICS-REMO2015', 'rcm': 'MOHC-HadREM3-GA7-05', 'ens': 'r1i1p1'},
+            ],
+        }
+    },
 }
