3159fcd5d95c1197eeb84c78d77fb4985b9b1f6c
Karim Bahgat <karim.bahgat.norway@gmail.com>
2026-04-27 16:07:08 +0200

    Add new data source download functions for optimized DestinE versions of ERA5-Land, and the coarser ERA5 dataset  (#10)
    * add destine era5-land hourly download, tests, and dependencies
    
    * add destine era5 (land and ocean) 25km data and test
    
    * drop support for py 3.10 since zarr requires >=3.11
    
    ---------
    
    Co-authored-by: Karim Bahgat <karimba@uio.no>

---
+++ dhis2eo/data/destine/era5/hourly.py
@@ -0,0 +1,150 @@
+import calendar
+import logging
+from pathlib import Path
+import os
+from datetime import date, timedelta
+
+import xarray as xr
+
+from ...utils import force_logging
+from ....utils.time import iter_months
+from ....utils.types import BBox, DateLike
+
+logger = logging.getLogger(__name__)
+force_logging(logger)
+
+
+# Requirements: 
+# Access to DestinE datasets requires registering an account with DestinE Earth Data Hub.
+# Free accounts have a monthly request limit of 500,000, and can be checked on user account page.
+# Authentication is handled by xarray and aiohttp and requires a .netrc (unix) or _netrc (windows) file
+# in user's home folder. 
+# see: https://earthdatahub.destine.eu/getting-started
+
+# Note:
+# Full dataset details and list of variables:
+# https://earthdatahub.destine.eu/collections/era5/datasets/reanalysis-era5-single-levels
+
+
+# Internal functions to get data from zarr
+def open_zarr(variables):
+    # zarr path
+    zarr_path = "https://data.earthdatahub.destine.eu/era5/reanalysis-era5-single-levels-v0.zarr"
+
+    # open with xarray
+    logger.info(f'Opening zarr archive from {zarr_path}')
+    ds = xr.open_dataset(
+        zarr_path,
+        storage_options={"client_kwargs":{"trust_env":True}},
+        chunks={},
+        engine="zarr",
+    )
+
+    # subset to only the specified variables
+    ds = ds[variables]
+
+    return ds
+
+def get_zarr_region(ds, bbox):
+    # convert ds longitudes from 0 to 360 to -180 to 180
+    logger.info('Correcting longitude coords to range -180 to 180')
+    ds = ds.assign_coords(longitude=((ds.longitude + 180) % 360 - 180)).sortby("longitude")
+
+    # extract the coordinates from input bounding box
+    logger.info(f'Computing bbox to use for subsetting')
+    xmin, ymin, xmax, ymax = map(float, bbox)
+
+    # add padding to include edge pixels
+    xres = abs(float(ds.longitude.diff('longitude').median()))
+    yres = abs(float(ds.latitude.diff('latitude').median()))
+    xmin, xmax = xmin - xres, xmax + xres
+    ymin, ymax = ymin - yres, ymax + yres
+
+    # filter to bbox coords
+    logger.info(f'Subsetting zarr archive to bbox with padding: {xmin} {ymin} {xmax} {ymax}')
+    ds = ds.sel(longitude=slice(xmin, xmax), latitude=slice(ymax, ymin))
+
+    return ds
+
+def get_zarr_month(ds, year, month):
+    # turn time query parameters into xarray selection
+    _, last_day = calendar.monthrange(year, month)
+    from_date = f"{year}-{month:02d}-01"
+    to_date = f"{year}-{month:02d}-{last_day:02d}"
+    
+    logger.info(f'Subsetting zarr archive to date range: {from_date} to {to_date}')
+    ds = ds.sel(valid_time=slice(from_date, to_date))
+
+    return ds
+
+
+# Public API to retrieve data for bbox between start and end date
+def download(
+    start: DateLike,
+    end: DateLike,
+    bbox: BBox,
+    dirname: str,
+    prefix: str,
+    variables: list[str],
+    overwrite: bool = False,
+):
+    """
+    Retrieves ERA5 hourly climate data for a given bbox, variables, and start/end dates.
+    Saves to disk in monthly files, as specified by dirname and prefix.
+    Returns list of file paths where data was downloaded, e.g. to use with xr.open_mfdataset().
+    """
+    os.makedirs(dirname, exist_ok=True)
+
+    # Parse dates
+    start_year, start_month = map(int, start.split('-')[:2])
+    end_year, end_month = map(int, end.split('-')[:2])
+
+    # Determine last date for which we can expect ERA5-Land to be complete
+    # DestinE's ERA5-Land seems to have roughly 15 days of lag
+    # Meaning only on the 15th of a new month, can we expect that the previous month contains all days
+    current_date = date.today()
+    last_updated_date = current_date - timedelta(days=15)
+
+    # Begin monthly downloads
+    zarr = None
+    files = []
+    for year, month in iter_months(start_year, start_month, end_year, end_month):
+        logger.info(f'Month {year}-{month}')
+
+        # Skip if month is expected to be incomplete
+        # DestinE updates the full previous month on the 15th
+        if (year,month) >= (last_updated_date.year, last_updated_date.month):
+            logger.warning(
+                f'Skipping downloads for months that are expected to be incomplete (~15 days of lag). '
+                f'Latest available date expected in ERA5: {last_updated_date.isoformat()}'
+            )
+            continue
+
+        # Determine the save path
+        save_file = f'{prefix}_{year}-{str(month).zfill(2)}.nc'
+        save_path = (Path(dirname) / save_file).resolve()
+        files.append(save_path)
+
+        # Download or use existing file
+        if overwrite is False and save_path.exists():
+            # File already exist, load from file instead
+            logger.info(f'File already downloaded: {save_path}')
+        
+        else:
+            if zarr is None:
+                # Open the full zarr archive
+                zarr = open_zarr(variables)
+
+                # Restrict to bbox
+                zarr = get_zarr_region(zarr, bbox)
+
+            # Fetch zarr data for the month
+            ds = get_zarr_month(zarr, year=year, month=month)
+
+            # Save to target path
+            logger.info("Saving data from DestinE Earth Data Hub...")
+            logger.info(f'--> {ds.dims}')
+            ds.to_netcdf(save_path)
+
+    # return list of all file downloads
+    return files
---
+++ tests/data/test_destine.py
@@ -0,0 +1,147 @@
+import logging
+from pathlib import Path
+from datetime import date, timedelta
+import pytest
+
+import geopandas as gpd
+import xarray as xr
+
+from dhis2eo.data.destine import era5, era5_land
+from dhis2eo.utils.time import months_ago
+
+DATA_DIR = Path(__file__).parent.parent / "test_data"
+
+# set verbose logging (hacky for now)
+logging.basicConfig(
+    level=logging.INFO,  # or DEBUG for more details
+)
+
+
+@pytest.mark.integration
+def test_download_hourly_era5land_data():
+    # download args
+    dirname = DATA_DIR / '../test_outputs/destine'
+    prefix = 'era5land_hourly_sierra_leone'
+
+    # get bbox
+    geojson_file = DATA_DIR / "sierra-leone-districts.geojson"
+    org_units = gpd.read_file(geojson_file)
+    bbox = org_units.total_bounds
+
+    # start/end dates
+    start = '2025-01'
+    end = '2025-06'
+
+    # download
+    variables = ['t2m', 'tp']
+    paths = era5_land.hourly.download(start, end, bbox, dirname=dirname, prefix=prefix, 
+                                      variables=variables, overwrite=True)
+    logging.info(paths)
+    assert len(paths) == 6
+
+    # test opening multifile xarray
+    ds = xr.open_mfdataset(paths)
+    logging.info(ds)
+
+    # test aggregating temperature to daily
+    daily_temp = ds['t2m'].resample(valid_time='1D').mean().compute()
+    logging.info(daily_temp)
+
+    # test visualize
+    #from earthkit.plots import quickplot
+    #fig = quickplot(ds.sel(valid_time=start))
+    #fig.save(dirname / 'era5land.png')
+
+
+@pytest.mark.integration
+def test_download_hourly_era5land_skip_incomplete_month():
+    # download args
+    dirname = DATA_DIR / '../test_outputs/cds'
+    prefix = 'era5land_hourly_sierra_leone'
+
+    # get bbox
+    geojson_file = DATA_DIR / "sierra-leone-districts.geojson"
+    org_units = gpd.read_file(geojson_file)
+    bbox = org_units.total_bounds
+
+    # start/end dates (current and previous month only)
+    current_date = date.today()
+    last_month = months_ago(current_date, 1)
+    start = last_month.isoformat()[:7]
+    end = current_date.isoformat()[:7]
+    logging.info(f'Today is {current_date.isoformat()}')
+    logging.info(f'Testing months {start} to {end}')
+
+    # download
+    variables = ['t2m', 'tp']
+    paths = era5_land.hourly.download(start, end, bbox, dirname=dirname, prefix=prefix, 
+                                      variables=variables, overwrite=False)
+    logging.info(paths)
+
+    # at least the current month should not be downloaded
+    # if it's earlier than the 15th day, then the previous month will also be skipped
+    # this means either 0 or 1 months should be downloaded and returned
+    assert len(paths) < 2
+
+
+@pytest.mark.integration
+def test_download_hourly_era5_data():
+    # download args
+    dirname = DATA_DIR / '../test_outputs/destine'
+    prefix = 'era5_hourly_sierra_leone'
+
+    # get bbox
+    geojson_file = DATA_DIR / "sierra-leone-districts.geojson"
+    org_units = gpd.read_file(geojson_file)
+    bbox = org_units.total_bounds
+
+    # start/end dates
+    start = '2025-01'
+    end = '2025-03'
+
+    # download
+    variables = ['t2m', 'tp']
+    paths = era5.hourly.download(start, end, bbox, dirname=dirname, prefix=prefix, 
+                                      variables=variables, overwrite=False)
+    logging.info(paths)
+    assert len(paths) == 3
+
+    # test opening multifile xarray
+    ds = xr.open_mfdataset(paths)
+    logging.info(ds)
+
+    # test aggregating temperature to daily
+    daily_temp = ds['t2m'].resample(valid_time='1D').mean().compute()
+    logging.info(daily_temp)
+
+    # test visualize
+    #from earthkit.plots import quickplot
+    #fig = quickplot(ds.sel(valid_time=start))
+    #fig.save(dirname / 'era5.png')
+
+
+# @pytest.mark.integration
+# def test_download_monthly_era5_data():
+#     # download args
+#     dirname = DATA_DIR / '../test_outputs/cds'
+#     prefix = 'era5_monthly_sierra_leone'
+
+#     # get bbox
+#     geojson_file = DATA_DIR / "sierra-leone-districts.geojson"
+#     org_units = gpd.read_file(geojson_file)
+#     bbox = org_units.total_bounds
+
+#     # start/end dates
+#     start = '2020'
+#     end = '2025'
+
+#     # download
+#     variables = ['2m_temperature', 'total_precipitation']
+#     paths = era5_land.monthly.download(start, end, bbox, dirname=dirname, prefix=prefix, 
+#                                       variables=variables, overwrite=True)
+#     logging.info(paths)
+#     assert len(paths) == 1
+
+#     # test opening the data
+#     ds = xr.open_dataset(paths[0])
+#     logging.info(ds)
