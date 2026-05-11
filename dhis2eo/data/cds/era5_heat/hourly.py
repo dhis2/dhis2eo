b025ea88eb4d6d65e370929ba1c0db9f14ae61b2
Karim Bahgat <karim.bahgat.norway@gmail.com>
2026-05-11 11:57:12 +0200

    Add era5-heat dataset with test (#16)
    Co-authored-by: Karim Bahgat <karimba@uio.no>

---
+++ dhis2eo/data/cds/era5_heat/hourly.py
@@ -0,0 +1,145 @@
+import calendar
+import json
+import logging
+from pathlib import Path
+import os
+from datetime import date, timedelta
+import time
+import tempfile
+import zipfile
+
+from ecmwf.datastores import Client
+import xarray as xr
+
+from ...utils import force_logging
+from ....utils.time import iter_months
+from ....utils.types import BBox, DateLike
+
+logger = logging.getLogger(__name__)
+force_logging(logger)
+
+
+# Internal function to execute a single monthly file download (API only allows one month at a time)
+def save_month(client, save_path, year, month, bbox, variables, use_server_cache):
+    # extract the coordinates from input bounding box
+    xmin, ymin, xmax, ymax = map(float, bbox)
+
+    # construct the query parameters
+    _, last_day = calendar.monthrange(year, month)
+    days = [day for day in range(1, last_day + 1)]
+    days = [str(day).zfill(2) for day in days]
+    params = {
+        "variable": variables,
+        "version": "1_1",
+        "product_type": "intermediate_dataset",
+        "variable": variables,
+        "year": [str(year)],
+        "month": [str(month).zfill(2)],
+        "day": days,
+        "time": [f"{str(h).zfill(2)}:00" for h in range(0, 23 + 1)],
+        "area": [ymax, xmin, ymin, xmax],  # notice how we reordered the bbox coordinate sequence
+    }
+
+    # if use_server_cache is False, add tiny numeric flag to invalidate request hash
+    # see: https://forum.ecmwf.int/t/how-to-avoid-the-cds-cache-issue/905/2
+    if not use_server_cache:
+        unique_numeric_string = str(int(time.time()))
+        params['nocache'] = unique_numeric_string
+
+    # download the data
+    logger.info("Downloading data from CDS API...")
+    logger.info(f"Request parameters: \n{json.dumps(params)}")
+    remote = client.submit(
+        "derived-utci-historical",
+        params
+    )
+
+    # download comes as zipfile with one nc file per day
+    # save to temp dir, extract, and consolidate to single nc file
+    with tempfile.TemporaryDirectory(delete=True) as tmpdir:
+        # download zipfile
+        zip_path = Path(tmpdir) / 'tempzip.zip'
+        remote.download(zip_path)
+
+        # extract all files to same folder
+        with zipfile.ZipFile(zip_path) as archive:
+            archive.extractall(tmpdir)
+
+        # open all extracted nc files
+        nc_paths = str(Path(tmpdir) / '*.nc')
+        ds = xr.open_mfdataset(nc_paths)
+
+        # save as single nc file and close file connections before cleanup
+        ds.to_netcdf(save_path)
+        ds.close()
+
+
+# Public API to retrieve data for bbox between start and end date
+def download(
+    start: DateLike,
+    end: DateLike,
+    bbox: BBox,
+    dirname: str,
+    prefix: str,
+    variables: list[str],
+    use_server_cache: bool = True,
+    overwrite: bool = False,
+):
+    """
+    Retrieves ERA5-HEAT hourly climate data for a given bbox, variables, and start/end dates.
+    Saves to disk in monthly files, as specified by dirname and prefix.
+    Returns list of file paths where data was downloaded, e.g. to use with xr.open_mfdataset().
+    """
+    os.makedirs(dirname, exist_ok=True)
+
+    # Parse dates
+    start_year, start_month = map(int, start.split('-')[:2])
+    end_year, end_month = map(int, end.split('-')[:2])
+
+    # Determine last date for which we can expect ERA5-HEAT to be complete
+    # ERA5-HEAT seems to have roughly 6-7 days of lag
+    # Meaning only on the 7th of a new month, can we expect that the previous month contains all days
+    current_date = date.today()
+    last_updated_date = current_date - timedelta(days=7)
+
+    # Create ecmwf client
+    client = Client()
+    client.check_authentication()
+
+    # Begin downloads
+    # NOTE: Although ecmwf-datastores allows asynch jobs, era5-heat is limited to max 1 runnning job
+    # ...so just doing it regular synchronously
+    files = []
+    for year, month in iter_months(start_year, start_month, end_year, end_month):
+        logger.info(f'Month {year}-{month}')
+
+        # Skip if month is expected to be incomplete
+        # Technically speaking CDS allows us to download monthly files with only some of the days.
+        # However, this introduces the issue of caching partial monthly downloads, where we would have to check if
+        # ...each file contains all days.
+        # As a simple solution, we instead check if the month is expected to be complete (based on the reported publishing lag of ERA5-HEAT)
+        # ...and issue a warning that we don't download incomplete months. 
+        # I think this should be fine in the DHIS2 context where reporting tends to happen for each month.
+        if (year,month) >= (last_updated_date.year, last_updated_date.month):
+            logger.warning(
+                f'Skipping downloads for months that are expected to be incomplete (~7 days of lag).'
+                f'Latest available date expected in ERA5-HEAT: {last_updated_date.isoformat()}'
+            )
+            continue
+
+        # Determine the save path
+        save_file = f'{prefix}_{year}-{str(month).zfill(2)}.nc'
+        save_path = (Path(dirname) / save_file).resolve()
+        files.append(save_path)
+
+        # Download or use existing file
+        if overwrite is False and save_path.exists():
+            # File already exist, load from file instead
+            logger.info(f'File already downloaded: {save_path}')
+        
+        else:
+            # Submit job request and save results to target path
+            save_month(client=client, save_path=save_path, year=year, month=month, bbox=bbox, variables=variables, use_server_cache=use_server_cache)
+
+    # return list of all file downloads
+    return files
